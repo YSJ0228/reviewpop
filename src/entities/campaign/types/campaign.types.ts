8af349824a728a89bda9eed9c59ba8c3dae49a22
solmin <smin2020@icloud.com>
2025-11-13 17:44:47 +0900

    feat: campaign 타입 수정

--- src/entities/campaign/types/campaign.types.ts
+++ src/entities/campaign/types/campaign.types.ts
@@ -37,6 +37,37 @@ export type CampaignCategory =
   | '기타'; // other
 
 /**
+ * 체험 유형
+ */
+export type ExperienceType = 'delivery' | 'visit';
+
+/**
+ * 후기 플랫폼
+ */
+export type ReviewPlatform = 'naver_blog' | 'other';
+
+/**
+ * ISO(8601)
+ */
+export type DateRange = [string, string];
+
+export interface CampaignSchedule {
+  applicationSchedule: DateRange;
+  winnerAnnouncementSchedule: DateRange;
+  reviewSchedule: DateRange;
+}
+
+/**
+ * 상세 정보 섹션
+ */
+export interface CampaignDescription {
+  /** 섹션 제목 */
+  title: string;
+  /** 섹션 내용 */
+  content: string;
+}
+
+/**
  * 캠페인 기본 정보
  */
 export interface Campaign {
@@ -47,7 +78,7 @@ export interface Campaign {
   /** 브랜드명 */
   brand: string;
   /** 캠페인 설명 */
-  description: string;
+  description: CampaignDescription;
   /** 카테고리 */
   category: CampaignCategory;
   /** 캠페인 상태 */
@@ -59,15 +90,8 @@ export interface Campaign {
   /** 상세 이미지 URL 배열 */
   imageUrls?: string[];
 
-  // 날짜
-  /** 캠페인 시작일 (ISO 8601) */
-  startDate: string;
-  /** 캠페인 종료일 (ISO 8601) */
-  endDate: string;
-  /** 신청 마감일 (ISO 8601) */
-  applicationDeadline: string;
-  /** 리뷰 작성 마감일 (ISO 8601) */
-  reviewDeadline?: string;
+  // 진행 일정
+  schedule: CampaignSchedule;
 
   // 지역 (선택 사항)
   /** 지역 제한 (예: "서울", "전국") */
@@ -92,26 +116,62 @@ export interface Campaign {
   createdAt: string;
 }
 
+/** 09:00 등 */
+type TimeString = string;
+type DailyHours = [TimeString, TimeString] | 'closed';
+
 /**
  * 캠페인 상세 정보
  * (목록에서는 필요 없지만 상세 페이지에서 필요한 정보)
  */
+
+//TODO: 예약, 방문 관련 더 추가
+export interface VisitReservation {
+  /** 영업시간 */
+  businessHours: [
+    DailyHours,
+    DailyHours,
+    DailyHours,
+    DailyHours,
+    DailyHours,
+    DailyHours,
+    DailyHours,
+  ];
+  /** 예약 유무 */
+  isReservationRequired: boolean;
+  /** 예약 방법 */
+  reservationMethod?: string;
+
+  visitReservationNotice?: string;
+}
+
 export interface CampaignDetail extends Campaign {
+  // 체험 정보
+  /** 체험 상품의 예상 가치 (원 단위) */
+  estimatedValue?: number;
+  /** 체험 유형 (배송/방문/둘 다) */
+  experienceType: ExperienceType;
+  /** 키워드/태그 목록 */
+  keywords: string[];
+
+  visitReservation?: VisitReservation;
+
   /** 리뷰 미션 목록 */
   reviewMission: string[];
-  /** 제공 항목 목록 */
-  providedItems: string[];
+  /** 주의 사항 */
+  reviewMissionNotice?: string;
+
   /** 배송 정보 */
   deliveryInfo?: {
     shippingDate?: string;
     trackingNumber?: string;
   };
-  /** 주의 사항 목록 */
-  notices?: string[];
-  /** 신청 조건 목록 */
+  /** 당첨 조건 목록 */
   requirements?: string[];
   /** 문의처 */
   contactInfo?: string;
+  /** 체험 시 주의사항 */
+  experiencePrecautions?: string[];
 }
 
 /**
