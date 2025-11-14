06a13d228f3e1639c37d40167e85a9fd6c565e63
Hyun <ikimyh059554@gmail.com>
2025-11-14 18:08:23 +0900

    refactor: 유효하지 않은 날짜에 대한 유효성 검사 로직 추가
    - 개발 환경에서만 에러 로깅하도록 추가
    - 잘못된 날짜가 전달되는 경우, dateText: "날짜 오류" 리턴

--- src/shared/components/ConfirmationCard/util.ts
+++ src/shared/components/ConfirmationCard/util.ts
@@ -1,10 +1,8 @@
-import dayjs from 'dayjs';
+import dayjs from '@shared/lib/dayjs.config';
 
-import type { DateInput } from '@shared/lib/date';
+import type { DateInput } from '@shared/lib/date.ts';
 
 import type { ConfirmationCardType } from './type';
-
-dayjs.locale('ko');
 
 /**
  * 타입별 날짜 포맷팅 함수
@@ -18,6 +16,16 @@ dayjs.locale('ko');
  */
 export function formatDateForConfirmationCard(date: DateInput, type: ConfirmationCardType) {
   const dayjsDate = dayjs(date);
+
+  if (!dayjsDate.isValid()) {
+    if (process.env.NODE_ENV === 'development') {
+      console.error('Invalid date:', date);
+    }
+    return {
+      dateText: '날짜 오류',
+      resultText: type === 'application' ? '에 선정 결과가 발표돼요' : null,
+    };
+  }
 
   if (type === 'reservation') {
     const dateStr = dayjsDate.format('M월 D일');
