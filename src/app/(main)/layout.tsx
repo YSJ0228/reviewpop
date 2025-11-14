ce8613100cab286aa1199c3dc7c0fed844024362
two678 <two6788@gmail.com>
2025-11-14 16:34:04 +0900

    feat: 공통 컴포넌트 Gnb 생성
    - 로고 이미지 추가 (/public/images/Logo.svg)
    - 로고와 알림 클릭 시 각각 홈, 알림 페이지로 이동

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,8 +1,10 @@
 import { BottomNavigation } from '@shared/components/BottomNavigation';
+import { Gnb } from '@shared/components/Gnb';
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <Gnb />
       <div style={{ paddingBottom: 'calc(60px + env(safe-area-inset-bottom))' }}>{children}</div>
       <BottomNavigation />
     </>
---
+++ src/shared/components/Gnb/index.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import type { GnbProps } from './types';
+import { IconNotification } from '@pop-ui/foundation';
+
+import styles from './style.module.scss';
+
+export function Gnb({ notification = 0 }: GnbProps) {
+  let notifiStyles = '';
+
+  if (notification > 0) {
+    notifiStyles = styles.notificationIconActive;
+    if (notification >= 9) {
+      notifiStyles = styles.notificationIconActivePlus;
+    }
+  }
+
+  return (
+    <header className={styles.Gnb}>
+      <Link href="/" className={styles.Gnb__Logo}>
+        <Image src="/images/Logo.svg" alt="로고 이미지" width={146} height={36} />
+      </Link>
+      <Link
+        href="/notifications"
+        className={styles.Notification__Icon}
+        aria-label="알림 페이지로 이동"
+      >
+        <IconNotification size={24} />
+        {notification > 0 && <span className={notifiStyles}>{notification}</span>}
+      </Link>
+    </header>
+  );
+}
