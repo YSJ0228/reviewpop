73dccd94011dac672ffa825211e2e9fe825a1c89
two678 <two6788@gmail.com>
2025-11-14 17:45:52 +0900

    style: gnb 타설명 추가

--- src/shared/components/Gnb/types.ts
+++ src/shared/components/Gnb/types.ts
@@ -1,4 +1,10 @@
 export interface GnbProps {
-  /** 알림 개수 (0 이상의 정수) */
+  /**
+   * 알림 개수
+   * - 음수는 0으로 처리됨
+   * - 소수점은 내림 처리됨
+   * - 10개 이상일 때 "9+"로 표시됨
+   * @default 0
+   */
   notification?: number;
 }
