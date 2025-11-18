a10ece1b0de3eb013fd247e7193b905551bea9a9
Hyun <ikimyh059554@gmail.com>
2025-11-18 17:19:06 +0900

    feat: 내 체험의 미선정 체험내역 구현

---
+++ src/features/history/components/RejectedList/index.tsx
@@ -0,0 +1,37 @@
+import { CampaignCard } from '@features/history/components/CampaignCard';
+import styles from './style.module.scss';
+import { useMyCampaigns } from '@entities/history/hooks/useMyCampaigns';
+
+export function RejectedList() {
+  const { data: campaigns, isLoading, error } = useMyCampaigns();
+
+  if (isLoading) {
+    return <div>로딩중...</div>;
+  }
+
+  if (error) {
+    return (
+      <div>
+        <p>데이터를 불러오는데 실패했습니다.</p>
+      </div>
+    );
+  }
+
+  if (campaigns?.length === 0) {
+    return (
+      <div>
+        <p>미선정 캠페인이 없습니다.</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.RejectedList}>
+      {campaigns
+        ?.filter((campaign) => campaign.status === 'rejected')
+        .map((campaign) => (
+          <CampaignCard key={campaign.id} campaign={campaign} type="rejected" />
+        ))}
+    </div>
+  );
+}
