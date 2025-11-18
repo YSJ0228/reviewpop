83f77b6b02da74f817dc70b6b9623117e1068b35
solmin <smin2020@icloud.com>
2025-11-18 16:58:20 +0900

    feat: campaign-card 및 campaign-list 스타일 변경

--- src/features/campaign/components/CampaignCard/index.tsx
+++ src/features/campaign/components/CampaignCard/index.tsx
@@ -1,43 +1,57 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import { formatDate } from '@shared/lib/date';
+import { diff, formatDate } from '@shared/lib/date';
 import type { CampaignCardProps } from './types';
 import styles from './style.module.scss';
+import { IconArrowRight } from '@pop-ui/foundation';
 
 export function CampaignCard({ campaign }: CampaignCardProps) {
+  const givenTime = campaign.schedule.applicationSchedule[1];
+  const currentTime = new Date();
+  const diffInDays = diff(givenTime, currentTime, 'day');
+  const applicationSchedule = campaign.schedule.applicationSchedule.map((d) =>
+    formatDate(d, 'SHORT'),
+  );
+
   return (
     <Link href={`/campaign/${campaign.id}`} className={styles.CampaignCard__Link}>
       <article className={styles.CampaignCard} aria-label={`${campaign.brand} ${campaign.title}`}>
-        <div className={styles.CampaignCard__ImageWrapper}>
-          <Image
-            src={campaign.imageUrl}
-            alt={`${campaign.brand} ${campaign.title} 체험 이미지`}
-            fill
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            style={{ objectFit: 'cover' }}
-          />
-          <div className={styles.CampaignCard__Badge} aria-label={`상태: ${campaign.location}`}>
-            {campaign.location}
-          </div>
+        <Image
+          src={campaign.imageUrl}
+          alt={`${campaign.brand} ${campaign.title} 체험 이미지`}
+          fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          style={{ objectFit: 'cover' }}
+        />
+        <div className={styles.CampaignCard__Badge} aria-label={`상태: ${campaign.location}`}>
+          {campaign.location}
         </div>
-        <div className={styles.CampaignCard__Content}>
-          <p className={styles.CampaignCard__Brand}>{campaign.brand}</p>
-          <h3 className={styles.CampaignCard__Title}>{campaign.title}</h3>
-          <h3 className={styles.CampaignCard__Title}>{campaign.providedItems.join(' + ')}</h3>
-          <p className={styles.CampaignCard__Brand}>며칠 남음</p>
-          <p className={styles.CampaignCard__Brand}>
-            {`신청 ${campaign.currentRecruitment}명/${campaign.maxRecruitment}명`}
-          </p>
-          <p className={styles.CampaignCard__Brand}>
-            체험단 모집 {campaign.schedule.applicationSchedule.join(' ~ ')}
-          </p>
-          <div className={styles.CampaignCard__Meta}>
-            <time dateTime={campaign.schedule.applicationSchedule[0]}>
-              신청일: {formatDate(campaign.schedule.applicationSchedule[0], 'SHORT')}
-            </time>
-            <time dateTime={campaign.schedule.applicationSchedule[1]}>
-              마감: {formatDate(campaign.schedule.applicationSchedule[1], 'SHORT')}
-            </time>
+        <div className={styles.CampaignCard__Wrapper}>
+          <div className={styles.CampaignCard__Header}>
+            <p className={styles.CampaignCard__Brand}>{campaign.brand}</p>
+            <h3 className={styles.CampaignCard__Title}>{campaign.title}</h3>
+            <h3 className={styles.CampaignCard__Items}>{campaign.providedItems.join(' + ')}</h3>
+          </div>
+          <div className={styles.CampaignCard__Content}>
+            <div>
+              <div className={styles.CampaignCard__Recruitment}>
+                <p>
+                  <b>{diffInDays}일 남음</b>
+                </p>
+                <p>
+                  <b>{`신청 ${campaign.currentRecruitment}명`}</b>
+                  {`/${campaign.maxRecruitment}명`}
+                </p>
+              </div>
+              <div className={styles.CampaignCard__Meta}>
+                <time dateTime={campaign.schedule.applicationSchedule.join()}>
+                  체험단 모집 {applicationSchedule.join(' ~ ')}
+                </time>
+              </div>
+            </div>
+            <button className={styles.CampaignCard__Button}>
+              <IconArrowRight color="white" />
+            </button>
           </div>
         </div>
       </article>
