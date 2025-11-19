5aa666e16d201b854da2c54eba407622093e9034
choi-mk <xhrlmin10@naver.com>
2025-11-19 13:05:08 +0900

    feat: apply-page 컴포넌트 구현

--- src/app/campaign/[campaignId]/apply/page.tsx
+++ src/app/campaign/[campaignId]/apply/page.tsx
@@ -1,10 +1,13 @@
 'use client';
 
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 
 import { ErrorBoundary } from '@shared/components/ErrorBoundary';
 
 import styles from './page.module.scss';
+import { LabeledInput } from '@shared/components/LabeledInput';
+import { WebButton } from '@shared/components/WebButton';
+import { CampaignApplyCard } from '@features/campaign/components/CampaignApplyCard';
 
 /**
  * 캠페인 신청 페이지
@@ -19,17 +22,42 @@ import styles from './page.module.scss';
  * 5. [ ] 에러 처리 (이미 신청한 경우, 마감된 경우 등)
  */
 export default function CampaignApplyPage({ params }: { params: { campaignId: string } }) {
+  const [text, setText] = useState<string>('');
+  const [phone, setPhone] = useState<string>('');
+  const [buttonType, setButtonType] = useState<'connect' | 'edit'>('connect');
+  const [isBottomSheet, setIsBottomSheet] = useState<boolean>(false);
   return (
     <main className={styles.CampaignApplyPage}>
       <ErrorBoundary>
         <Suspense fallback={<div>로딩 중...</div>}>
           {/* TODO: ApplyForm 컴포넌트 추가 */}
-          <div className={styles.Placeholder}>
-            <p>캠페인 ID: {params.campaignId}</p>
-            <p>캠페인 신청 폼</p>
-            <p className={styles.Todo}>
-              features/campaign/components/ApplyForm 컴포넌트를 구현하세요
-            </p>
+          <CampaignApplyCard
+            brand="그라운드 220"
+            size="sm"
+            providedItems={['티셔츠 2개 제작 체험', '하이볼 2잔 무료 체험']}
+          />
+          <div className={styles.CampaignApplyPage__ApplyForm}>
+            <WebButton
+              label="네이버 블로그 주소"
+              buttonType={buttonType}
+              onClick={() => setIsBottomSheet(true)}
+            />
+            <LabeledInput
+              label="이름"
+              text={text}
+              setText={setText}
+              inputType="name"
+              placeholder="이름을 입력해주세요"
+            />
+            <LabeledInput
+              label="전화번호"
+              text={phone}
+              setText={setPhone}
+              isButton={true}
+              isText={true}
+              inputType="url"
+              placeholder="01012345678"
+            />
           </div>
         </Suspense>
       </ErrorBoundary>
---
+++ src/features/campaign/components/CampaignApplyCard/index.tsx
@@ -0,0 +1,28 @@
+import { CampaignApplyCardProps } from './types';
+
+import styles from './style.module.scss';
+
+export function CampaignApplyCard({ size = 'sm', brand, providedItems }: CampaignApplyCardProps) {
+  const itemsText = providedItems.join(' + ');
+
+  const styleCard =
+    size === 'sm'
+      ? { height: 52, paddingTop: 8, paddingLeft: 16 }
+      : { height: 103, justifyContent: 'center', alignItems: 'center', gap: 8 };
+
+  const styleBrand =
+    size === 'sm'
+      ? { fontSize: 12, fontWeight: 600, color: 'var(--gray-900)', lineHeight: '18px' }
+      : { fontSize: 16, fontWeight: 700, color: 'var(--gray-800)' };
+  const styleItems =
+    size === 'sm'
+      ? { fontSize: 12, fontWeight: 500, color: 'var(--gray-600)', lineHeight: '18px' }
+      : { fontSize: 14, fontWeight: 500, color: 'var(--gray-800)' };
+
+  return (
+    <div className={styles.CampaignApplyCard} style={styleCard}>
+      <span style={styleBrand}>{brand}</span>
+      <span style={styleItems}>{itemsText}</span>
+    </div>
+  );
+}
