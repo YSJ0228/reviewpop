86e3cfcc3b69f9a79873fe0d6614aa65f218ef0c
choi-mk <xhrlmin10@naver.com>
2025-11-19 14:54:24 +0900

    refactor: 주석 추가, button config 상수 추가

--- src/shared/components/WebButton/constants.ts
+++ src/shared/components/WebButton/constants.ts
@@ -1,7 +1,30 @@
 import { TButtonType } from './types';
 
-export const BUTTON_LABEL: Record<TButtonType, string> = {
-  copy: '복사',
-  edit: '수정',
-  connect: '연결하기',
+type TButtonConfigItem = {
+  image: string;
+  label: string;
+  class: string;
+};
+
+export const BUTTON_CONFIG: Record<TButtonType, TButtonConfigItem> = {
+  copy: {
+    image: '',
+    label: '복사',
+    class: `WebButton__Button--Copy`,
+  },
+  edit: {
+    image: '/images/BlogLogo.svg',
+    label: '수정',
+    class: `WebButton__Button--Edit`,
+  },
+  connect: {
+    image: '/images/BlogLogo.svg',
+    label: '연결하기',
+    class: `WebButton__Button--Connect`,
+  },
 } as const;
+
+export const TEXT = {
+  placeholder: '블로그를 연결해주세요',
+  connectedLabel: '연결됨',
+};
