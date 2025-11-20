9d81dfa4eadc3bf2fe496222a528d7046d0b0d96
solmin <smin2020@icloud.com>
2025-11-20 13:11:18 +0900

    refactor: useCountdown 음수 출력 방지

--- src/shared/hooks/useCountdown.ts
+++ src/shared/hooks/useCountdown.ts
@@ -6,7 +6,7 @@ export function useCountdown(targetDate: string) {
   const [timeLeft, setTimeLeft] = useState<number>(0);
 
   useEffect(() => {
-    const update = () => setTimeLeft(diff(targetDate, now(), 'second'));
+    const update = () => setTimeLeft(Math.max(0, diff(targetDate, now(), 'second')));
     update();
     const timer = setInterval(() => update(), 1000);
 
