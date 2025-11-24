f6941eb5e82f1773c1f48dca22dfd6d13ee74575
two678 <two6788@gmail.com>
2025-11-24 18:29:29 +0900

    feat: 예약 정보 확인 카드 스타일 구현

--- src/features/reserve/components/ReserveConfirm/index.tsx
+++ src/features/reserve/components/ReserveConfirm/index.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 import { useEffect } from 'react';
 import { useReservationStore } from '@features/reserve/store/reservationStore';
 import { mockReservationData } from '@features/reserve/store/mockReservationData';
@@ -19,35 +20,34 @@ export function ReserveConfirm({ campaignId }: { campaignId: string }) {
     <div className={styles.ReserveConfirm}>
       <div className={styles.ReserveConfirm__Info}>
         <div className={styles.ReserveConfirm__Info__Title}>
-          <img
-            src={reservationData.thumbnailUrl}
-            alt="예약한 체험 이미지"
-            width={150}
-            height={150}
-          />
+          <img src={reservationData.thumbnailUrl} alt="예약한 체험 이미지" />
           <div className={styles.ReserveConfirm__Info__Title__Text}>
             <h3>{reservationData.brand}</h3>
             <p>{reservationData.providedItem}</p>
           </div>
         </div>
-        <div className={styles.ReserveConfirm__Info__Address}>
+        <div className={(styles.ReserveConfirm__Info__Address, styles.BorderBottom)}>
           <span>주소</span>
-          <p>{reservationData.address}</p>
-          <p>{reservationData.bookerPhone}</p>
-          <button>네이버 블로그</button>
+          <div className={styles.ReserveConfirm__Info__Address__Detail}>
+            <p>{reservationData.address}</p>
+            <button>
+              <Image src="/images/NaverMap.png" alt="네이버 맵" width={20} height={20} />
+            </button>
+          </div>
+        </div>
+        <div className={(styles.ReserveConfirm__Info__Date, styles.BorderBottom)}>
+          <span>날짜</span>
+          <p>{reservationData.visitDate}</p>
+          <p>{reservationData.visitTime}</p>
+        </div>
+        <div className={(styles.ReserveConfirm__Info__Visitor, styles.BorderBottom)}>
+          <span>방문 인원</span>
+          <p>{reservationData.visitorCounter} 명</p>
         </div>
         <div className={styles.ReserveConfirm__Info__Booker}>
           <span>예약자 정보</span>
           <p>{reservationData.bookerName}</p>
-        </div>
-        <div className={styles.ReserveConfirm__Info__Visitor}>
-          <span>방문 인원</span>
-          <p>{reservationData.visitorCounter}</p>
-        </div>
-        <div className={styles.ReserveConfirm__Info__Date}>
-          <span>날짜</span>
-          <p>{reservationData.visitDate}</p>
-          <p>{reservationData.visitTime}</p>
+          <p>{reservationData.bookerPhone}</p>
         </div>
       </div>
       <div className={styles.ReserveConfirm__Precautions}>
