a2307445528e077d7c164b48c30fbae98702ed37
solmin <smin2020@icloud.com>
2025-11-26 15:53:21 +0900

    feat: campaign-list 정리

--- src/features/campaign/components/CampaignList/CompletedCampaignList.tsx
+++ src/features/campaign/components/CampaignList/CompletedCampaignList.tsx
@@ -7,15 +7,15 @@ import type { CampaignListProps } from './types';
 import styles from './style.module.scss';
 
 export function CompletedCampaignList({ filteredCampaigns }: Omit<CampaignListProps, 'status'>) {
+  const sortedCampaigns = filteredCampaigns.sort((a, b) =>
+    diff(b.schedule.winnerAnnouncementSchedule[1], a.schedule.winnerAnnouncementSchedule[1]),
+  );
+
   return (
     <div className={styles.CampaignList} role="feed" aria-label="마감된 체험 목록">
-      {filteredCampaigns
-        .sort((a, b) =>
-          diff(b.schedule.winnerAnnouncementSchedule[1], a.schedule.winnerAnnouncementSchedule[1]),
-        )
-        .map((campaign) => (
-          <CompletedCard key={campaign.id} campaign={campaign} />
-        ))}
+      {sortedCampaigns.map((campaign) => (
+        <CompletedCard key={campaign.id} campaign={campaign} />
+      ))}
     </div>
   );
 }
