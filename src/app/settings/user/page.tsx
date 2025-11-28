51dfbd45af9b0d4b7f557254c238042cbca0e71e
choi-mk <xhrlmin10@naver.com>
2025-11-28 14:49:10 +0900

    feat: 개인정보 페이지 생성

---
+++ src/app/settings/user/page.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { Suspense } from 'react';
+
+import { ErrorBoundary } from '@shared/components/ErrorBoundary';
+
+import styles from './page.module.scss';
+import { Settings } from '@features/profile/components/Settings';
+
+/**
+ * 설정 페이지
+ * - 하단 탭: X
+ * - 진입점: 프로필 페이지 > 설정 버튼
+ *
+ * TODO:
+ * 1. [ ] Settings 컴포넌트 구현 (@features/profile/components/Settings)
+ * 2. [ ] 알림 설정
+ * 3. [ ] 개인정보 설정
+ * 4. [ ] 로그아웃 버튼
+ * 5. [ ] 회원탈퇴 버튼
+ * 6. [ ] 앱 정보 (버전, 이용약관, 개인정보처리방침)
+ */
+export default function UserPage() {
+  return (
+    <main className={styles.UserPage}>
+      <ErrorBoundary>
+        <Suspense fallback={<div>로딩 중...</div>}></Suspense>
+      </ErrorBoundary>
+    </main>
+  );
+}
