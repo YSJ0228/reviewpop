dd32ec3e296a59fcc42d45b737be688e1fd72913
Sejun <sam8192@naver.com>
2025-11-28 23:41:27 +0900

    feat: 체험 상세 페이지 당첨 조건 컴포넌트 구현

--- src/features/campaign/index.ts
+++ src/features/campaign/index.ts
@@ -12,6 +12,7 @@ export { CampaignContents } from './components/CampaignContents';
 export { CampaignValue } from './components/CampaignValue';
 export { CampaignInfoSection } from './components/CampaignInfoSection';
 export { CampaignScheduleSection } from './components/CampaignScheduleSection';
+export { CampaignRequirementsSection } from './components/CampaignRequirementsSection';
 
 // Hooks
 export { useCampaigns } from '@entities/campaign/hooks/useCampaigns';
@@ -29,3 +30,4 @@ export type { CampaignContentsProps } from './components/CampaignContents';
 export type { CampaignValueProps } from './components/CampaignValue';
 export type { CampaignInfoSectionProps } from './components/CampaignInfoSection';
 export type { CampaignScheduleSectionProps } from './components/CampaignScheduleSection';
+export type { CampaignRequirementsSectionProps } from './components/CampaignRequirementsSection';
