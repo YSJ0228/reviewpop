1b612c297283d67acca18e55b00150d362565584
two678 <two6788@gmail.com>
2025-12-01 15:15:03 +0900

    style: 주석 제거

--- src/app/campaign/[campaignId]/reserve/confirm/page.tsx
+++ src/app/campaign/[campaignId]/reserve/confirm/page.tsx
@@ -9,19 +9,6 @@ import { ReserveConfirm } from '@features/reserve/components/ReserveConfirm';
 
 import styles from './page.module.scss';
 
-/**
- * 예약 정보 확인 페이지
- * - 하단 탭: X
- * - 입력한 예약 정보 확인 및 최종 제출
- *
- * TODO:
- * 1. [ ] ReserveConfirm 컴포넌트 구현 (@features/reserve/components/ReserveConfirm)
- * 2. [ ] 예약 정보 요약 표시 (날짜, 시간, 방문자 정보)
- * 3. [ ] 최종 제출 API 연동 (useReserve 훅)
- * 4. [ ] 제출 완료 시 /campaign/[id]/reserve/complete로 리다이렉트
- * 5. [ ] 수정 버튼 (이전 페이지로 돌아가기)
- */
-
 interface ReserveConfirmPageProps {
   params: Promise<{ campaignId: string }>;
 }
