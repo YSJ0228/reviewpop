e221dac0f17778058f0321e7a145bc9080b2f870
choi-mk <xhrlmin10@naver.com>
2025-12-01 16:32:55 +0900

    feat: 개인정보 페이지 구현

--- src/app/settings/user/page.tsx
+++ src/app/settings/user/page.tsx
@@ -3,9 +3,14 @@
 import { Suspense } from 'react';
 
 import { ErrorBoundary } from '@shared/components/ErrorBoundary';
+import { useUserInfo } from '@entities/user/hooks/useUserInfo';
 
 import styles from './page.module.scss';
-import { Settings } from '@features/profile/components/Settings';
+
+import { Form } from '@shared/components/Form';
+import { useUpdateUserInfo } from '@entities/user/hooks/useUpdateUserInfo';
+import { FormDataType } from '@shared/components/Form/types';
+import { useRouter } from 'next/navigation';
 
 /**
  * 설정 페이지
@@ -21,10 +26,20 @@ import { Settings } from '@features/profile/components/Settings';
  * 6. [ ] 앱 정보 (버전, 이용약관, 개인정보처리방침)
  */
 export default function UserPage() {
+  const updateUser = useUpdateUserInfo();
+  const router = useRouter();
+
+  const handleSave = (formData: FormDataType) => {
+    updateUser.mutate(formData);
+    router.push('/settings');
+  };
+
   return (
     <main className={styles.UserPage}>
       <ErrorBoundary>
-        <Suspense fallback={<div>로딩 중...</div>}></Suspense>
+        <Suspense fallback={<div>로딩 중...</div>}>
+          <Form onClick={handleSave} showTextArea={false} />
+        </Suspense>
       </ErrorBoundary>
     </main>
   );
