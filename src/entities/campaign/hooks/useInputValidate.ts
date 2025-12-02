cc64549a657a77a4451d8da8fb94050de01b771d
two678 <two6788@gmail.com>
2025-12-02 16:10:45 +0900

    refactor: 테스트를 위해 mock 데이터 수정

--- src/entities/campaign/hooks/useInputValidate.ts
+++ src/entities/campaign/hooks/useInputValidate.ts
@@ -3,12 +3,15 @@ import { useState } from 'react';
 // 정규식을 상수로 분리
 const VALIDATION_PATTERNS = {
   url: /^[a-z0-9-_]{5,30}$/,
+  blogUrl: /^https:\/\/blog\.naver\.com\/[a-z0-9-_]{5,30}\/[0-9]+$/,
   name: /^[가-힣a-zA-Z\s]{1,20}$/,
   phone: /^01\d{8,9}$/,
 } as const;
 
 const ERROR_MESSAGES = {
   url: '블로그 아이디는 소문자 영문, 숫자, -, _ 만 사용 가능하며 5~30자여야 합니다.',
+  blogUrl:
+    '올바른 네이버 블로그 포스트 URL 형식이 아닙니다. (https://blog.naver.com/아이디/게시글번호)',
   name: '이름을 확인해주세요',
   phone: '전화번호를 확인해주세요',
 } as const;
